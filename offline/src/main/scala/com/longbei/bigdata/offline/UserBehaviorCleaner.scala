666693dbb77990703c688cf28d9cba94781a046a
starcaixx <247815709@qq.com>
2020-05-05 18:10:03 +0800

    解析日志入hive表格

--- offline/src/main/scala/com/longbei/bigdata/offline/UserBehaviorCleaner.scala
+++ offline/src/main/scala/com/longbei/bigdata/offline/UserBehaviorCleaner.scala
@@ -2,8 +2,12 @@ package com.longbei.bigdata.offline
 
 import java.net.URLDecoder
 
+import com.longbei.bigdata.comm.util.ConfigurationUtil
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
+import org.apache.hadoop.security.UserGroupInformation
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object UserBehaviorCleaner {
@@ -18,26 +22,80 @@ object UserBehaviorCleaner {
     val inputPath = args(0)
     val outputPath = args(1)
 
+    initKerberos()
+//    val conf = new SparkConf().setAppName(getClass.getSimpleName).setMaster("local[4]")
+//    val sc = new SparkContext(conf)
+    val session: SparkSession = SparkSession.builder().appName(getClass.getSimpleName).master("local[4]")
+  .enableHiveSupport().getOrCreate()
 
-    print(inputPath+":out:"+outputPath)
-    val conf = new SparkConf().setAppName(getClass.getSimpleName).setMaster("local[4]")
-    val sc = new SparkContext(conf)
-
-    val hdfs = org.apache.hadoop.fs.FileSystem.get(sc.hadoopConfiguration)
-    if (hdfs.exists(new Path(outputPath))) {
-      hdfs.delete(new Path(outputPath),true)
-    }
+    import session.implicits._
+    val sc = session.sparkContext
+//    val hdfs = org.apache.hadoop.fs.FileSystem.get(sc.hadoopConfiguration)
+//    if (hdfs.exists(new Path(outputPath))) {
+//      hdfs.delete(new Path(outputPath),true)
+//    }
 
     // 通过输入路径获取RDD
     val eventRDD: RDD[String] = sc.textFile(inputPath)
-
     // 清洗数据，在算子中不要写大量业务逻辑，应该将逻辑封装到方法中
-    eventRDD.filter(event => checkEventValid(event))  // 验证数据有效性
-      .map(event=>urlDecode(event))//url解码
-      .coalesce(3)
-      .saveAsTextFile(outputPath)
+    val logDS = eventRDD.filter(event => checkEventValid(event)) // 验证数据有效性
+      .map(event => urlDecode(event)) //url解码
+      .map(event => formatStr(event))
+      .toDS()
+    logDS
 
-    sc.stop()
+
+    println(logDS.count())
+
+
+//      .coalesce(1)
+//      .saveAsTextFile(outputPath)
+
+//    session.sql()
+    session.stop()
+//    sc.stop()
+  }
+
+  def initKerberos(): Unit ={
+    //kerberos权限认证
+//    val path = ConfigurationUtil.getClass.getClassLoader.getResource("").getPath
+    val path = "D:/work/gitlab/sparkcal/comm/src/main/resources/"
+//    print("path:"+path)
+    val principal = ConfigurationUtil.getValueByKey("kerberos.principal")
+    val keytab = ConfigurationUtil.getValueByKey("kerberos.keytab")
+
+    val conf = new Configuration
+    System.setProperty("java.security.krb5.conf", "C:/ProgramData/MIT/Kerberos5/krb5.ini")
+//    conf.addResource(new Path(path + "hbase-site.xml"))
+    conf.addResource(new Path(path + "hdfs-site.xml"))
+    conf.addResource(new Path(path + "hive-site.xml"))
+    conf.set("hadoop.security.authentication", "Kerberos")
+    conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
+    UserGroupInformation.setConfiguration(conf)
+    UserGroupInformation.loginUserFromKeytab(principal, path+keytab)
+    println("login user: "+UserGroupInformation.getLoginUser())
+  }
+
+  case class LogInfo(enType:String,logStr:String)
+
+  /**
+    * 格式化日志为type  log格式
+    * @param event
+    * @return
+    */
+  def formatStr(event: String):LogInfo={
+    val strPros = event.split("&")
+    var eventType=""
+    var logStr=""
+    strPros.foreach(str=>{
+      val props = str.split("=")
+      if ("ltype".equals(props(0))) {
+        eventType=props(1)
+      }else if ("log".equals(props(0))) {
+        logStr=props(1)
+      }
+    })
+    LogInfo(eventType,logStr)
   }
 
   /**
--- offline/src/test/java/Test.java
+++ offline/src/test/java/Test.java
@@ -3,15 +3,15 @@ import java.net.URLDecoder;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "61.151.206.18 -  [28/Apr/2020:14:32:55 +0800] \"GET /log HTTP/1.0\" 200 14 \"\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML; like Gecko) Mobile/12F70 MicroMessenger/6.1.5 NetType/WIFI\" \"\"\n";
-        String str1 = "124.202.181.170 -  [28/Apr/2020:14:32:04 +0800] \"POST /log HTTP/1.1\" 200 24 \"\" \"DragonCup/5.6.9 (iPhone; iOS 12.2; Scale/3.00)\" \"\" appkey=yingyin&enc=0&log=%7B%22tms%22%3A1%2C%22ip%22%3A%22192.168.0.104%22%2C%22province%22%3A%22%22%2C%22did%22%3A%2228371882e23f4e2a94d89d821473fb68%22%2C%22ts%22%3A1588055524.2391319%2C%22brand%22%3A%22Apple%22%2C%22imei%22%3A%2228371882e23f4e2a94d89d821473fb68%22%2C%22lng%22%3A0%2C%22wsize%22%3A0%2C%22cosid%22%3A%22%22%2C%22macad%22%3A%22%22%2C%22accu%22%3A0%2C%22os%22%3A%2212.2%22%2C%22unet%22%3A%22Wi-Fi%22%2C%22cid%22%3A%22%22%2C%22ver%22%3A%225.6.9%22%2C%22uid%22%3A%22941117597104029696%22%2C%22cpid%22%3A%22%22%2C%22model%22%3A%22iPhone%207%20Plus%22%2C%22wid%22%3A2594318685%2C%22lat%22%3A0%2C%22optype%22%3A%22pause%22%2C%22endpos%22%3A0%2C%22chl%22%3A%22AppStore%22%2C%22vdsize%22%3A0%7D&ltype=watchvideo";
-        System.out.println(str.split(" ").length);
-        System.out.println(str1.split(" ").length);
-        try {
-            String decode = URLDecoder.decode(str.split(" ")[17], "utf-8");
+//        String str = "61.151.206.18 -  [28/Apr/2020:14:32:55 +0800] \"GET /log HTTP/1.0\" 200 14 \"\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML; like Gecko) Mobile/12F70 MicroMessenger/6.1.5 NetType/WIFI\" \"\"\n";
+        String str1 = "enc=0&appkey=yingyin&ltype=watchvideo&log={\"accu\":\"93.0\",\"area\":\"北京市海淀区上地三街靠近金融科贸大厦\",\"brand\":\"Xiaomi\",\"chl\":\"longbei4.5\",\"country\":\"中国\",\"did\":\"longbei70:3A:51:3F:99:BD\",\"imei\":\"longbei70:3A:51:3F:99:BD\",\"ip\":\"192.168.0.36\",\"lat\":\"40.036172\",\"lng\":\"116.309734\",\"macad\":\"70:3A:51:3F:99:BD\",\"model\":\"MI PAD 4\",\"os\":\"8.1.0\",\"ts\":\"2020-04-30 10:01:55\",\"uid\":\"25121\",\"unet\":\"WIFI\",\"value\":{\"cid\":\"1249971917226631168\",\"cosid\":\"30193\",\"cpid\":\"1249972163868389376\",\"optype\":\"start\",\"stapos\":\"0\",\"uid\":\"25121\",\"vdsize\":\"269560\",\"wid\":\"1588212115304\"},\"ver\":\"5.6.9\"}";
+//        System.out.println(str.split(" ").length);
+        System.out.println(str1.split("&")[0]);
+        /*try {
+            String decode = URLDecoder.decode(str1.split("\t")[9], "utf-8");
             System.out.println(decode);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
